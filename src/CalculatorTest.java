e328bc2b37916a633b41b9b63ae829daac1bd318
ahmad <ahmad@DESKTOP-UIGS59O>
2020-02-27 23:19:17 +0200

    added calculator class, calculatorTest with basic test cases

---
+++ src/CalculatorTest.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+         int a=10;
+         int b=20;
+         Calculator calc=new Calculator();
+         int result=calc.sum(a,b);
+         Assert.assertTrue(result>0);
+	}
+
+}
