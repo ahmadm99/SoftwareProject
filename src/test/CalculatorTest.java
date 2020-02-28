eb3822e8af9cb552e498a34f25508ad23b7a4cc8
ahmad <ahmad@DESKTOP-UIGS59O>
2020-02-28 02:04:58 +0200

    a

--- src/test/CalculatorTest.java
+++ src/test/CalculatorTest.java
@@ -10,14 +10,21 @@ import softwareEng.Calculator;
 
 public class CalculatorTest {
 
-	//@Test
-	 void test() {
+	@Test
+	 public void test() {
 		//fail("Not yet implemented");
          int a=10;
          int b=20;
          Calculator calc=new Calculator();
          int result=calc.sum(a,b);
          Assert.assertTrue(result>0);
+	}
+	public void aa() {
+		int a=5;
+		int b=3;
+		Calculator calc=new Calculator();
+		int result=calc.sub(a,b);
+		Assert.assertTrue(result>0);
 	}
 
 }
